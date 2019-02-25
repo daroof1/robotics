73e5f6a3933f7381d10883e670b2f19cf2a44b4e
daroof1 <dhruv.loke@gmail.com>
2019-02-24 22:07:43 -0600

    Add files via upload

---
+++ NewCrater.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "NewCrater",group = "LinearOpMode")
+public class NewCrater extends CommonUtils{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Auto();
+    }
+}
---
+++ NewDepot.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "NewDepot",group = "LinearOpMode")
+public class NewDepot extends CommonUtils {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Auto();
+    }
+}
