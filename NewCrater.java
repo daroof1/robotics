37031f007732b4fa70fa0015bddee77d7ae5d007
daroof1 <dhruv.loke@gmail.com>
2019-02-25 22:07:46 -0600

    Add files via upload

--- NewCrater.java
+++ NewCrater.java
@@ -6,8 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "NewCrater",group = "LinearOpMode")
 public class NewCrater extends CommonUtils{
 
+
     @Override
     public void runOpMode() throws InterruptedException {
+        craterSide = true;
         Auto();
     }
 }
--- NewDepot.java
+++ NewDepot.java
@@ -6,8 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "NewDepot",group = "LinearOpMode")
 public class NewDepot extends CommonUtils {
 
+
     @Override
     public void runOpMode() throws InterruptedException {
+        craterSide = false;
         Auto();
     }
 }
